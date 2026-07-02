bbba57c3ed3ac39f8acf6d66a96ea9ce0048406e
sobhan <sobhanjafarii87@gmail.com>
2026-07-02 15:51:54 +0330

    [ADD] about me component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Content from "@/features/content/content";
 import Profile from "@/features/profile/profile";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col justify-start gap-5 sm:gap-7">
       <Profile />
+
+      <Content />
     </main>
   );
 }
