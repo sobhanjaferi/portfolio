ddeee9728ef2e189d1730923ccc1009f091a96d6
sobhan <sobhanjafarii87@gmail.com>
2026-07-06 20:26:55 +0330

    [ADD] skills component

--- src/features/aboutMe/aboutMe.tsx
+++ src/features/aboutMe/aboutMe.tsx
@@ -1,12 +1,6 @@
-import IconButton from "@/components/iconButton";
 import { ReactNode } from "react";
-import { FaGitAlt, FaJsSquare } from "react-icons/fa";
-import { FaReact } from "react-icons/fa6";
-import { IoLogoCss3 } from "react-icons/io";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { SiHtml5, SiManjaro, SiNextdotjs, SiTypescript } from "react-icons/si";
-import { FaSquareGithub } from "react-icons/fa6";
 import Cotntent from "@/components/content";
+import Skills from "./skills";
 
 function AboutMe(): ReactNode {
   return (
@@ -22,51 +16,7 @@ function AboutMe(): ReactNode {
 
       <br />
 
-      <section>
-        <h2 className="text-2xl font-bold mb-2">Skills</h2>
-
-        <div className="w-full flex justify-start items-center gap-3 overflow-x-auto pb-4">
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-orange-600">
-            <SiHtml5 />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-cyan-600">
-            <IoLogoCss3 />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-amber-400">
-            <FaJsSquare />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-cyan-600">
-            <FaReact />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-cyan-300">
-            <RiTailwindCssFill />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-black">
-            <SiNextdotjs />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-cyan-700">
-            <SiTypescript />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-orange-600">
-            <FaGitAlt />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[50px] text-black">
-            <FaSquareGithub />
-          </IconButton>
-
-          <IconButton className="min-w-30 h-30 rounded-lg bg-white/7 text-[45px] text-emerald-500">
-            <SiManjaro />
-          </IconButton>
-        </div>
-      </section>
+      <Skills />
     </Cotntent>
   );
 }
